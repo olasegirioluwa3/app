9ed4e2cdbfa29eb5426980e32c0cef1fb0832c8b
Glitch (curvy-sly-washer) <none>
2024-06-15 19:07:33 +0000

    😽🏣 Checkpoint ./index.js:65835507/715

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Importing necessary modules and packages
 import express from 'express';
 import { config } from 'dotenv';
 import fs from 'fs';
@@ -8,33 +7,20 @@ import OpenAI from 'openai';
 import axios from 'axios';
 import cors from 'cors';
 import { createServer } from 'http';
-// import socketIO from 'socket.io';
 import userRoutes from './routes/userRoutes.js';
 import whatsappRoutes from './routes/whatsappRoutes.js';
-// import applicationRoutes from './routes/applicationRoutes.js';
 import paymentRoutes from './routes/paymentRoutes.js';
 import serviceRoutes from './routes/serviceRoutes.js';
 import serviceAccessRoutes from './routes/serviceAccessRoutes.js';
 import userAccessRoutes from './routes/userAccessRoutes.js';
-// import { sequelize } from './models/index.js';
 import db from './models/index.js';
 const sequelize = db.sequelize;
 
-// Load environmen
-// import socketIO from 'socket.io';
 import http from 'http';
-// // Import error logging middleware
+// Import error logging middleware
 
-// // Use error logging middleware
+// Use error logging middleware
 const app = express();
-// const server = http.createServer(app);
-// const io = socketIO(server, {
-//   cors: {
-//     origin: ["https://zionrebornuniversity.com.ng","http://localhost:3001"], 
-//     methods: ['GET', 'POST'],
-//     credentials: true,
-//   },
-// });
 
 app.use(cors());
 app.use(express.json());
@@ -45,18 +31,10 @@ app.use('/uploads', express.static("public/uploads"));
 
 userRoutes(app, null, sequelize);
 whatsappRoutes(app, null, sequelize);
-// applicationRoutes(app, io, sequelize);
 paymentRoutes(app, null, sequelize);
 serviceRoutes(app, null, sequelize);
 serviceAccessRoutes(app, null, sequelize);
 userAccessRoutes(app, null, sequelize);
-
-// io.on('connection', (socket) => {
-//   console.log('A user connected');
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected');
-//   });
-// });
 
 app.get('/', (req, res) => {
   return res.status(201).json({ message: 'Welcome to ZionAI API' });
